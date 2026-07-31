0ed15846ce9a8c6420b80362684d9a18fe2a9bd4
Clownatique <132279628+Clownatique@users.noreply.github.com>
2026-07-31 04:49:04 +0200

    refactor(dmenu call):create function to make code cleaner

--- niri-outputs-dmenu.sh
+++ niri-outputs-dmenu.sh
@@ -1,39 +1,76 @@
 #/usr/bin/env bash
 
+menu() { fuzzel --dmenu --minimal-lines; }
+prompt() { fuzzel --dmenu --prompt-only $1; }
+
 outputs=$(niri msg --json outputs)
-choosen_output=$(echo $outputs | jq -r  '.[] | ."name" + " • " + ."make" | tostring' | fuzzel --dmenu --minimal-lines)
-names=$(echo $outputs | jq -r  '.[] | ."name" | tostring' )
+
+nirijq() {
+  # apply a filter to niri ipc outputs output
+  local name=$1
+  local filter=$2
+  index=0
+  if [ "${3+x}" = "x" ]; then
+    index=$3
+  fi
+  jq -r --arg name "$name" --argjson index $index "$filter" <<<"$outputs"
+}
+
+names=$(nirijq  "" '.[] | ."name" | tostring'  )
+
+choosen_output=$(nirijq ""  '.[] | ."name" + " • " + ."make" | tostring'  | menu)
+
 choosen_name=$(echo $choosen_output | sed 's/ •.*//g')
 
 # if [ -n $(echo $choosen_output | grep [a-z]* ) ]; then
 #    exit
 # fi
 
-current_mode_index=$(echo $outputs | jq -r --arg name $choosen_name '.[$name].current_mode')
-current_mode=$(echo $outputs | jq --argjson index $current_mode_index --arg name $choosen_name '.[$name].modes[0]| [.width,"x",.height,"@",.refresh_rate] |map_values(tostring)|add')
-current_vrr=$(niri msg --json outputs | jq --arg name $choosen_name -r '.[$name].vrr_supported')
-current_pos=$(niri msg --json outputs | jq --arg name $choosen_name -r '.[$name].logical | [.x, ",",.y]|map(tostring)|add')
-current_scale=$(niri msg --json outputs | jq --arg name $choosen_name -r '.[$name].logical.scale')
+current_mode_index=$(nirijq $choosen_name '.[$name].current_mode')
+current_mode=$(nirijq $choosen_name '.[$name].modes[$index]| [.width,"x",.height,"@",.refresh_rate] |map_values(tostring)|add' $current_mode_index)
+vrr_on_current=$(nirijq $choosen_name '.[$name].vrr_supported')
+current_pos=$(nirijq $choosen_name '.[$name].logical | [.x, ",",.y]|map(tostring)|add')
+current_scale=$(nirijq $choosen_name '.[$name].logical.scale')
+current_trans=$(nirijq $choosen_name '.[$name].logical.transform')
 
 setting_choice=$(echo -e "󰲎 :$current_mode\n󱥼 :$current_vrr\n󰓱 :$current_pos\n󰩨 :$current_scale"| fuzzel --dmenu --minimal-lines )
 
+
 case $setting_choice in
   󰲎*)
-    modes=$(niri msg --json outputs | jq -r --arg name $choosen_name '.[$name]."modes"[] |  [ .width,"x",.height,"@",.refresh_rate]|map_values(tostring)|add')
-    new_mod=$(printf '%s\n' "$modes"| fuzzel --dmenu --minimal-lines) # thanks chat :((
+    modes=$(nirijq $choosen_name '.[$name]."modes"[] |  [ .width,"x",.height,"@",.refresh_rate / 1000]|map_values(tostring)|add')
+    new_mod=$(printf '%s\n' "$modes"| menu ) # thanks chat :((
     niri msg output $choosen_name mode $new_mod
     ;;
   󱥼*)
-    #should do some check to ensure vrr is avaible
-    # do some check
-    # i dont use vrr so aha
+    if [[ $current_vrr = "false" ]]; then
+      niri msg output $choosen_name vrr on
+    else 
+      niri msg output $choosen_name vrr off
+    fi
   ;;
   󰓱*)
-    new_scale=$(fuzzel --dmenu --prompt-only "new position")
-    # do some check..
-    niri msg output $choosen_name scale $new_scale ;;
+    new_pos=$(prompt "new position")
+    echo $new_pos
+    if [[ $new_pos =~ [0-9],[0-9] ]]; then
+      niri msg output $choosen_name scale $new_pos
+    else
+      echo error | menu
+    fi
+    ;;
   󰩨*)
-    new_scale=$(fuzzel --dmenu --prompt-only "new scale")
-    # do some check..
-    niri msg output $choosen_name scale $new_scale ;;
+    new_scale=$(prompt "new scale")
+    if [[ $new_scale =~ [0-9]+\.?[0-9]* ]]; then
+      niri msg output $choosen_name scale $new_scale
+    else
+      echo error | menu
+    fi;;
+  *)
+    new_trans=$(echo -e "normal\n90\n180\n270\nflipped\nflipped-90\nflipped-180\nflipped-270"|menu)
+      niri msg output $choosen_name transform $new_trans
+    # if [[ $new_trans =~ (^normal$)?(flipped-)?[0-9][0-9]+ ]]; then
+    # else
+    #   echo error | menu
+    # fi
+  ;;
 esac
